61e0b081e4ff8a681863b204fcdb785c05a10e67
Pedro-Henrique-Costa <pedrohcosta0209@gmail.com>
2023-05-19 23:05:26 -0300

    feat: created background, shadow, seted height and width in the HomePageContent

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Header from "./components/Header"
+import HomePageContent from "./components/HomePageContent"
 
 function App() {
 
   return (
     <>
       <Header/>
+      <HomePageContent/>
     </>
   )
 }
---
+++ src/components/HomePageContent/index.tsx
@@ -0,0 +1,12 @@
+import styles from "./index.module.css"
+
+const HomePageContent = () => {
+    return (
+
+        <section className={styles.section_container}>
+            
+        </section>
+    )
+}
+
+export default HomePageContent;
