d5399e68dd13388fc8fbeeeaf13970ef55398e2d
Pedro-Henrique-Costa <pedrohcosta0209@gmail.com>
2023-05-21 00:04:51 -0300

    feat: create media for tablets, min-width: 768px

--- src/components/HomePageContent/index.tsx
+++ src/components/HomePageContent/index.tsx
@@ -2,8 +2,12 @@ import styles from "./index.module.css"
 
 //Img imports
 import design_background from "../../assets/Component 3.svg"
+import pikachu_completo from "../../assets/BannerComplete.svg"
 
 const HomePageContent = () => {
+
+    const { innerWidth: width } = window;
+
     return (
 
         <section className={styles.section_container}>
@@ -12,8 +16,8 @@ const HomePageContent = () => {
 
                 <article className={styles.text_content}>
 
-                    <h1> <b>Find</b> all your favorite <b>Pokemon</b></h1>
-                    <h2>You can know the type of Pokemon, its strengths, disadvantages and abilities</h2>
+                    <h1 className={styles.text_content_h1}> <b>Find</b> all your favorite <b>Pokemon</b></h1>
+                    <h2 className={styles.text_content_h2}>You can know the type of Pokemon, its strengths, disadvantages and abilities</h2>
 
                     <button className={styles.button}>See Pokemons</button>
 
@@ -21,8 +25,8 @@ const HomePageContent = () => {
 
                 <article>
 
-                    <img className={styles.img} src={design_background} alt="" />
-                    
+                    <img className={styles.img} src={width <= 1080? pikachu_completo : design_background} alt=""/>
+
                 </article>
 
             </main>
