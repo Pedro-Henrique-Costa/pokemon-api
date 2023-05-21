abb41a9ab79442d68cf7e663397b57e6dd4051a2
Pedro-Henrique-Costa <pedrohcosta0209@gmail.com>
2023-05-21 19:33:27 -0300

    feat: create a media for mobile

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,23 @@
 import styles from "./index.module.css"
 import logo from "../../assets/logo.svg"
+import burguerBtn from "../../assets/BurgenBtn.svg"
 
 const Header = () => {
+
+    const { innerWidth: width } = window;
+
     return (
 
         <header className={styles.header}>
 
             <section className={styles.section}>
-                
+
                 <img className={styles.img} src={logo} alt="Logo da serie de jogos e anime Pokemon" />
+
+                <a className={width <= 768 ? styles.notHidenn : styles.isHidden}>
+                    <img src={burguerBtn} alt="Botão para mobile" />
+                </a>
+
 
                 <nav className={styles.nav}>
                     <button className={styles.button}>Home</button>
